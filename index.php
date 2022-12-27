0bfd25ec76fcd0c5a7c0931abadd86d386af46b6
iqrokristiadi <iqro.kristiadi2004@gmail.com>
2022-12-27 20:47:45 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -15,5 +15,10 @@
 	cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
 	proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
 
+	<ul>
+		<li>satu</li>
+		<li>dua</li>
+	</ul>
+
 </body>
 </html>
